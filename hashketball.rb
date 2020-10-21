1f1264355be00c2c1ad0980ca028ff1a1bad0151
Benjamin Stein <whereyouben@gmail.com>
2020-10-21 17:59:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,7 @@ require 'pry'
 def game_hash
   hash = {:home => {:team_name => "Brooklyn Nets", 
              :colors => ["Black", "White"], 
-             :players => [:alan_anderson => {player_name: "Alan Anderson",
-                                             number: 0,
+             :players => ["Alan Anderson" => {number: 0,
                                              shoe: 16,
                                              points: 22,
                                              rebounds: 12,
